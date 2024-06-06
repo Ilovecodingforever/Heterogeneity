adb13d1ad473c5b847ee870c54e98205eb90a88d
Ilovecodingforever <mingzliu@umich.edu>
2024-06-06 14:42:35 -0400

    print tree

--- src/model.py
+++ src/model.py
@@ -270,7 +270,7 @@ def predict_CMHE(model, x):
 
 
 
-def plot_tree(features, phenotypes, name=''):
+def plot_tree(features, phenotypes, name='', preprocessor=None):
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(features, phenotypes)
     plt.figure(figsize=(50, 10))
@@ -281,6 +281,83 @@ def plot_tree(features, phenotypes, name=''):
     plt.close()
 
     print('depth of tree', clf.get_depth())
+
+    # if preprocessor is not None:
+    #     mean = preprocessor.scaler.scaler.mean_
+    #     std = np.sqrt(preprocessor.scaler.scaler.var_)
+
+    # n_nodes = clf.tree_.node_count
+    # children_left = clf.tree_.children_left
+    # children_right = clf.tree_.children_right
+    # feature = clf.tree_.feature
+    # threshold = clf.tree_.threshold
+    # values = clf.tree_.value
+
+    # node_depth = np.zeros(shape=n_nodes, dtype=np.int64)
+    # is_leaves = np.zeros(shape=n_nodes, dtype=bool)
+    # stack = [(0, 0)]  # start with the root node id (0) and its depth (0)
+    # while len(stack) > 0:
+    #     # `pop` ensures each node is only visited once
+    #     node_id, depth = stack.pop()
+        
+    #     node_depth[node_id] = depth
+
+    #     # If the left and right child of a node is not the same we have a split
+    #     # node
+    #     is_split_node = children_left[node_id] != children_right[node_id]
+    #     # If a split node, append left and right children and depth to `stack`
+    #     # so we can loop through them
+    #     if is_split_node:
+    #         stack.append((children_left[node_id], depth + 1))
+    #         stack.append((children_right[node_id], depth + 1))
+    #     else:
+    #         is_leaves[node_id] = True
+
+    # print(
+    #     "The binary tree structure has {n} nodes and has "
+    #     "the following tree structure:\n".format(n=n_nodes)
+    # )
+    # for i in range(n_nodes):
+        
+    #     # if node_depth[i] > 3:
+    #     #     continue
+        
+    #     if is_leaves[i]:
+    #         print(
+    #             "{space}node={node} is a leaf node with value={value}.".format(
+    #                 space=node_depth[i] * "\t", node=i, value=values[i]
+    #             )
+    #         )
+    #     else:
+    #         thres = threshold[i]
+    #         if features.columns[feature[i]] == 'age':
+    #             thres = thres*std[0] + mean[0]
+    #         if features.columns[feature[i]] == 'bmi':
+    #             thres = thres*std[1] + mean[1]
+            
+    #         print(
+    #             # "{space}node={node} is a split node with value={value}: "
+    #             "{space}node={node}: "
+    #             # "go to node {left} if X[:, {feature}] <= {threshold} "
+    #             "go to node {left} if {feature_name} <= {threshold} "
+    #             "else to node {right}.".format(
+    #                 space=node_depth[i] * "\t",
+    #                 node=i,
+    #                 left=children_left[i],
+    #                 # feature=feature[i],
+    #                 feature_name=features.columns[feature[i]],
+    #                 threshold=thres,
+    #                 right=children_right[i],
+    #                 value=values[i],
+    #             )
+    #         )
+            
+    return
+
+
+
+
+
 
 
 
@@ -303,8 +380,11 @@ def phenotyping(outcomes_raw, features_raw, treatment, cat_feats, num_feats, mod
     # standardize, one hot encode
     x = features.copy()
     preprocessor = Preprocessor(cat_feat_strat='ignore', num_feat_strat='mean')
-    processed = preprocessor.fit_transform(x[cat_feats + num_feats], cat_feats=cat_feats, num_feats=num_feats,
-                                    one_hot=True, fill_value=-1).astype(float)
+    # processed = preprocessor.fit_transform(x[cat_feats + num_feats], cat_feats=cat_feats, num_feats=num_feats,
+    #                                 one_hot=True, fill_value=-1).astype(float)
+    preprocessor = preprocessor.fit(x[cat_feats + num_feats], cat_feats=cat_feats, num_feats=num_feats,
+                                    one_hot=True, fill_value=-1)
+    processed = preprocessor.transform(x[cat_feats + num_feats]).astype(float)    
     x.drop(cat_feats + num_feats, axis=1, inplace=True)
     x[processed.columns] = processed
 
@@ -320,7 +400,7 @@ def phenotyping(outcomes_raw, features_raw, treatment, cat_feats, num_feats, mod
             treatment=treatment, name=name)
 
     # plot tree
-    plot_tree(x, phenotypes, name=name)
+    plot_tree(x, phenotypes, name=name, preprocessor=preprocessor)
 
     return model
 
